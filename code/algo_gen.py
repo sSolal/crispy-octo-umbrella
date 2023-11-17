1b31d1a50f23a92fb288c6bb4db72b6372afd376
jcauvi <jcauvi@gitlab.aliens-lyon.fr>
2023-11-17 12:13:57 +0100

    Genom

--- code/algo_gen.py
+++ code/algo_gen.py
@@ -1,8 +1,27 @@
-from simulation import Graph
+from graphs import Graph
 
-class Genom(Graph):
-	def __init__(self):
-	    print("Hello World")
+class Genom:
+	def __init__(self, n,weights=[]):
+        self.n=n
+        if weights==[]:
+            self.weights=[1]*n
+        else:
+            self.weights=weights
+            self.n=len(self.weights)
+
+    def random_genom(self):
+        for i in range(self.n):
+            self.weight[i]=random.random()
 	    
 	def mutate(self):
 	    pass#...
+
+class Population:
+    def __init__(self, G, N):
+        self.G = G
+        self.pop=[]
+        for i in range(N):
+            self.pop.append(Genom(len(self.G.edges)))
+
+P = Genom(10)
+enfant = P.mutate()
